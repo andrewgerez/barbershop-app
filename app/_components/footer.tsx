243748ebba55f1d632b801d6837855bebeb22842
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-01 00:23:45 -0300

    feat: popular barbershops (init)

---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-secondary py-6 px-5">
+      <p className="text-gray-400 font-light opacity-65">
+        © 2024 Copyright BarberShop
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
