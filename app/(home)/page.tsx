76c3fb5b66fea0ba5e18c8adcb667394c71ea51c
Andrew Gerez <andrewgerez18@gmail.com>
2024-01-31 21:05:03 -0300

    feat: search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,25 @@
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
+  const currentDate = format(new Date(), "EEEE',' dd 'de' MMMM", {
+    locale: ptBR,
+  })
+
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Andrew!</h2>
+        <p className="capitalize text-xs font-light">{currentDate}</p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
     </div>
   );
 }
