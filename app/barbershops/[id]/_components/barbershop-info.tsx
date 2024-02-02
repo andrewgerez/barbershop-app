7a189f3d399931900bb24c18affdf7c9f7ad4508
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-01 23:18:28 -0300

    feat: barbershop info component

---
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { BarbershopType } from "@/app/types";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+interface IBarbershopInfo {
+  barbershop: BarbershopType;
+}
+
+const BarbershopInfo = ({ barbershop }: IBarbershopInfo) => {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.back();
+  }
+
+  return (
+    <div>
+      <div className="h-[250px] w-full relative">
+        <Button 
+          size="icon" 
+          variant="outline" 
+          className="z-50 absolute top-4 left-4"
+          onClick={handleBackClick}
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <Button 
+          size="icon" 
+          variant="outline" 
+          className="z-50 absolute top-4 right-4"
+        >
+          <MenuIcon />
+        </Button>
+
+        <Image 
+          src={barbershop.imageUrl}
+          alt={barbershop.name}
+          style={{ objectFit: "cover" }}
+          className="opacity-75"
+          fill
+        />
+      </div>
+
+      <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
+        <h1 className="text-xl font-bold">{barbershop.name}</h1>
+        <div className="flex items-center gap-1 mt-2">
+          <MapPinIcon 
+            className="text-primary"
+            size={18}
+          />
+          <p className="text-sm font-light">{barbershop.address}</p>
+        </div>
+        <div className="flex items-center gap-1 mt-2">
+          <StarIcon 
+            className="text-primary"
+            size={18}
+          />
+          <p className="text-sm font-light">5,0 (300 avaliações)</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default BarbershopInfo;
