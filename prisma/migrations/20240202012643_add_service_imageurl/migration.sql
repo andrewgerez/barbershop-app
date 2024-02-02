f5a8c0a3fe3d790c1d5017f4a7d428a100ce3595
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-01 22:43:58 -0300

    feat: new imageurl on service

---
+++ prisma/migrations/20240202012643_add_service_imageurl/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Service" ADD COLUMN     "imageUrl" TEXT;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -59,31 +59,36 @@ async function seedDatabase() {
         name: "Corte de Cabelo",
         description: "Estilo personalizado com as últimas tendências.",
         price: 60.0,
-        imageUrl: "https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png",
+        imageUrl:
+          "https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png",
       },
       {
         name: "Barba",
         description: "Modelagem completa para destacar sua masculinidade.",
         price: 40.0,
-        imageUrl: "https://utfs.io/f/e6bdffb6-24a9-455b-aba3-903c2c2b5bde-1jo6tu.png",
+        imageUrl:
+          "https://utfs.io/f/e6bdffb6-24a9-455b-aba3-903c2c2b5bde-1jo6tu.png",
       },
       {
         name: "Pézinho",
         description: "Acabamento perfeito para um visual renovado.",
         price: 35.0,
-        imageUrl: "https://utfs.io/f/8a457cda-f768-411d-a737-cdb23ca6b9b5-b3pegf.png",
+        imageUrl:
+          "https://utfs.io/f/8a457cda-f768-411d-a737-cdb23ca6b9b5-b3pegf.png",
       },
       {
         name: "Sobrancelha",
         description: "Expressão acentuada com modelagem precisa.",
         price: 20.0,
-        imageUrl: "https://utfs.io/f/2118f76e-89e4-43e6-87c9-8f157500c333-b0ps0b.png",
+        imageUrl:
+          "https://utfs.io/f/2118f76e-89e4-43e6-87c9-8f157500c333-b0ps0b.png",
       },
       {
         name: "Massagem",
         description: "Relaxe com uma massagem revigorante.",
         price: 50.0,
-        imageUrl: "https://utfs.io/f/c4919193-a675-4c47-9f21-ebd86d1c8e6a-4oen2a.png",
+        imageUrl:
+          "https://utfs.io/f/c4919193-a675-4c47-9f21-ebd86d1c8e6a-4oen2a.png",
       },
       {
         name: "Hidratação",
@@ -118,7 +123,7 @@ async function seedDatabase() {
                 id: barbershop.id,
               },
             },
-            imageUrl: service.imageUrl
+            imageUrl: service.imageUrl,
           },
         });
       }
