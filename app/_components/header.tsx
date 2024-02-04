67a3b86853111b20d2343e55e60f7372b9f45bff
Andrew Gerez <andrewgerez18@gmail.com>
2024-02-04 03:46:35 -0300

    feat: sidemenu component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,22 +1,11 @@
-"use client";
 import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
-import { Calendar, HomeIcon, LogInIcon, LogOutIcon, MenuIcon, UserIcon } from "lucide-react";
-import { signIn, signOut, useSession } from "next-auth/react";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
-import { Avatar } from "./ui/avatar";
-import { AvatarImage } from "@radix-ui/react-avatar";
-import Link from "next/link";
+import { MenuIcon } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./side-menu";
 
 const Header = () => {
-  const { data } = useSession();
-  const handleLoginClick = async () => {
-    await signIn('google');
-  }
-
-  const handleLogoutClick = () => signOut();
-
   return (
     <Card className="border-none">
       <CardContent className="p-5 justify-between items-center flex flex-row">
@@ -29,53 +18,7 @@ const Header = () => {
           </SheetTrigger>
 
           <SheetContent className="p-0">
-            <SheetHeader className="text-left border-b border-solid border-secondary p-5">
-              <SheetTitle>Menu</SheetTitle>
-            </SheetHeader>
-            {data?.user ? (
-              <div className="flex items-center justify-between px-5 py-6">
-                <div className="flex items-center gap-3">
-                  <Avatar>
-                    <AvatarImage src={data.user?.image ?? ''} />
-                  </Avatar>
-
-                  <h2 className="font-bold">{data.user.name}</h2>
-                </div>
-                <Button variant="secondary" size="icon" onClick={handleLogoutClick}>
-                  <LogOutIcon />
-                </Button>
-              </div>
-            ) : (
-              <div className="flex flex-col px-5 py-6 gap-3">
-                <div className="flex items-center gap-3">
-                  <UserIcon size={32} />
-                  <h2 className="font-bold">Olá, faça seu login!</h2>
-                </div>
-
-                <Button variant="secondary" className="w-full justify-start" onClick={handleLoginClick}>
-                  <LogInIcon className="mr-2" size={18} />
-                  Fazer Login
-                </Button>
-              </div>
-            )}
-
-            <div className="flex flex-col gap-3 px-5">
-              <Button variant="outline" className="justify-start" asChild>
-                <Link href="/">
-                  <HomeIcon size={18} className="mr-2" />
-                  Início
-                </Link>
-              </Button>
-
-              {data?.user && (
-                <Button variant="outline" className="justify-start" asChild>
-                  <Link href="/bookings">
-                    <Calendar size={18} className="mr-2" />
-                    Agendamentos
-                  </Link>
-                </Button>
-              )}
-            </div>
+            <SideMenu />
           </SheetContent>
         </Sheet>
       </CardContent>
--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { BarbershopType } from "@/app/types";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
@@ -20,24 +22,28 @@ const BarbershopInfo = ({ barbershop }: IBarbershopInfo) => {
   return (
     <div>
       <div className="h-[250px] w-full relative">
-        <Button 
-          size="icon" 
-          variant="outline" 
+        <Button
+          size="icon"
+          variant="outline"
           className="z-50 absolute top-4 left-4"
           onClick={handleBackClick}
         >
           <ChevronLeftIcon />
         </Button>
 
-        <Button 
-          size="icon" 
-          variant="outline" 
-          className="z-50 absolute top-4 right-4"
-        >
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline"  className="z-50 absolute top-4 right-4">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
 
-        <Image 
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+
+        <Image
           src={barbershop.imageUrl}
           alt={barbershop.name}
           style={{ objectFit: "cover" }}
@@ -49,14 +55,14 @@ const BarbershopInfo = ({ barbershop }: IBarbershopInfo) => {
       <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
         <h1 className="text-xl font-bold">{barbershop.name}</h1>
         <div className="flex items-center gap-1 mt-2">
-          <MapPinIcon 
+          <MapPinIcon
             className="text-primary"
             size={18}
           />
           <p className="text-sm font-light">{barbershop.address}</p>
         </div>
         <div className="flex items-center gap-1 mt-2">
-          <StarIcon 
+          <StarIcon
             className="text-primary"
             size={18}
           />
@@ -66,5 +72,5 @@ const BarbershopInfo = ({ barbershop }: IBarbershopInfo) => {
     </div>
   );
 }
- 
+
 export default BarbershopInfo;
